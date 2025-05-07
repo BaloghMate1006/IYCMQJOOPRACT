ddbe6fceb954e64d3b2063c8c3a9a23be1d68bbb
BaloghMate1006 <zeusz007hun@gmail.com>
2025-05-07 21:33:54 +0200

    Add files via upload

---
+++ IYCMQJ_0320/Tomb.java
@@ -0,0 +1,11 @@
+
+
+public class Tomb {
+	public static void main(String[] args) {
+
+		String[] days = {"Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"};
+
+		System.out.println(days[3]);
+		System.out.println(days.length);
+	}
+}
---
+++ IYCMQJ_0320/Tomb0.java
@@ -0,0 +1,12 @@
+
+
+public class Tomb0 {
+	public static void main(String[] args) {
+
+		String[][] mondoka = {
+				{"Egy", "megérett a meggy"},
+				{"Kettő", "csipkebokor vessző"},
+				{"Három", "te vagy az én párom"}};
+		System.out.println(mondoka[2][1]);
+	}
+}
